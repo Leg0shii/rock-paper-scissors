179fdd22ea8b92494580ad82b9727315974846ff
0hnezahn <supidupi12@yahoo.de>
2023-01-09 15:46:31 +0100

    add game logic - computer random selection - player input selection - winner/loser logic - console output - static game loop

--- main.js
+++ main.js
@@ -1,1 +1,58 @@
-console.log("Hello World");
+function getComputerChoice() {
+    // determine value between 1 and 3 (including 1 and 3)
+    let randomValue = Math.floor(Math.random()*3)+1;
+
+    // get mapping for the values
+    if (randomValue == 1) return 'rock';
+    if (randomValue == 2) return 'paper';
+    return 'scissor';
+}
+
+function validatePlayerSelection(message) {
+    if (!message) return false;
+    if (message != 'rock' && message != 'scissor' && message != 'paper') return false;
+    return true;
+}
+
+function playRound(playerSelection, computerSelection) {
+    let parsedPlayerSelection = playerSelection.toLowerCase();
+
+    // player=rock he wins if computer has scissors
+    if (parsedPlayerSelection == 'rock') {
+        if (computerSelection == 'rock') return [1, 'A tie!']; 
+        else if (computerSelection == 'paper') return [0, 'You Lose! Paper beats Rock'];
+        else return [2, 'You Win! Rock beats Scissor'];
+    }
+    // player=paper he wins if computer has rock
+    if (parsedPlayerSelection == 'paper') {
+        if (computerSelection == 'paper') return [1, 'A tie!']; 
+        else if (computerSelection == 'scissor') return [0, 'You Lose! Scissor beats Paper'];
+        else return [2, 'You Win! Paper beats Rock'];
+    }
+    // player=scissors he wins if computer has paper
+    if (parsedPlayerSelection == 'scissor') {
+        if (computerSelection == 'scissor') return [1, 'A tie!']; 
+        else if (computerSelection == 'rock') return [0, 'You Lose! Rock beats Scissor'];
+        else return [2, 'You Win! Scissor beats Paper'];
+    }
+}
+
+function game() {
+    let playerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Enter your guess: 'rock', 'paper', 'scissor'!");
+        let computerSelection = getComputerChoice();
+
+        let result = playRound(playerSelection, computerSelection);
+        let winnerResult = result[0];
+        let messageResult = result[1];
+
+        playerScore = playerScore + winnerResult;
+        console.log('Round ' + (i+1) + '. ' + messageResult);
+    }
+
+    if (playerScore == 5) console.log("It's a Tie! :|");
+    if (playerScore < 5) console.log("You have Lost with a total of " + playerScore + "/10 points! :(");
+    if (playerScore > 5) console.log("You have Won with a total of " + playerScore + "/10 points! :)");
+}
